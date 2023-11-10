0b4085b6847c528dae54d704d8cc31b2e87ecf96
Johann <johann_antonio85@hotmail.com>
2023-11-09 19:18:31 -0600

    Refactored connection string to use user secrets.

--- blanquitaAPI/BlanquitaAPI/Data/TacosBlanquitaContext.cs
+++ blanquitaAPI/BlanquitaAPI/Data/TacosBlanquitaContext.cs
@@ -33,8 +33,14 @@ public partial class TacosBlanquitaContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=Tacos_Blanquita;Trusted_Connection=True;TrustServerCertificate=True");
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<TacosBlanquitaContext>();
+
+        var configuration = builder.Build();
+
+        optionsBuilder.UseSqlServer(configuration["ConnectionStrings:TacosBlanquitaContext"]);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
