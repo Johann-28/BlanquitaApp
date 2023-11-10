92214c6b64b7050b624e7b0bcaf3351836354037
Johann <johann_antonio85@hotmail.com>
2023-11-10 17:25:12 -0600

    Remove circular references to fix JSON serialization

--- blanquitaAPI/BlanquitaAPI/Program.cs
+++ blanquitaAPI/BlanquitaAPI/Program.cs
@@ -1,12 +1,16 @@
+using System.Text.Json.Serialization;
 using BlanquitaAPI.Data;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers().AddJsonOptions(options =>
-options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve);
+{
+  options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
