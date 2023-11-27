a35656a566bdd1a346331411713ea37a6128ce7f
Johann <johann_antonio85@hotmail.com>
2023-11-26 23:39:31 -0600

    Add HttpClientModule and create TipoProductoService

--- blanquitaApp/src/app/tab1/tab1.page.ts
+++ blanquitaApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TipoProductoService as TipoProductoService } from '../https/tipo-producto.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor(private productoService: TipoProductoService) {}
+  ngOnInit(): void {}
 
-  constructor() {}
-
+  public consultarTiposProductos() {
+    this.productoService.getApiData().subscribe((data) => console.log(data));
+  }
 }
