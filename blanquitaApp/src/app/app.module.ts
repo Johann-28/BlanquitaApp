653ee22f5530c22d7b6d4e956a01807a145aad5e
JuanCDiaz1 <83601765+JuanCDiaz1@users.noreply.github.com>
2023-11-29 17:05:36 -0600

    Tab Tipos Prodducto Terminada

--- blanquitaApp/src/app/app.module.ts
+++ blanquitaApp/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { BaseUrlInterceptor } from './auth/interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , HttpClientModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: HTTP_INTERCEPTORS, useClass: BaseUrlInterceptor, multi: true }],
   bootstrap: [AppComponent],
--- blanquitaApp/src/app/tab1/tab1.page.ts
+++ blanquitaApp/src/app/tab1/tab1.page.ts
@@ -1,16 +1,77 @@
-import { Component } from '@angular/core';
-import { TipoProductoService as TipoProductoService } from '../https/tipo-producto.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { TipoProductoService } from '../https/tipo-producto.service';
+import { ProductTypeDTO } from '../DTOs/products.dto';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-  constructor(private productoService: TipoProductoService) {}
-  ngOnInit(): void {}
+export class Tab1Page implements OnInit {
+  displayedColumns: string[] = ['Clave', 'Descripción', 'Edición'];
+  editDialogConfig = new MatDialogConfig();
+  productType = new ProductTypeDTO();
+  dataSource: ProductTypeDTO[] = [];
+  productTypes: ProductTypeDTO[] = [];
+  clave: string = '';
+  descripcion: string = '';
 
-  public consultarTiposProductos() {
-    this.productoService.getApiData().subscribe((data) => console.log(data));
+  constructor(
+    private dialog: MatDialog,
+    private tipoProductoService: TipoProductoService,
+    private cdr: ChangeDetectorRef
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.tipoProductoService.getApiData().subscribe((productTypesData: ProductTypeDTO[]) => {
+      this.productTypes = productTypesData;
+      this.dataSource = this.productTypes;
+    });
+  }
+
+  consultarTiposProductos() {
+    this.tipoProductoService.getApiData().subscribe((productTypesData: ProductTypeDTO[]) => {
+      this.productTypes = productTypesData;
+    });
+  }
+
+  isNotEmpty(): boolean {
+    return this.clave.length > 0 && this.descripcion.length > 0;
+  }
+
+  editProductType(row: any): void {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '30%',
+      height: '60%',
+      data: {
+        productType: row
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+        this.tipoProductoService.getApiData().subscribe((productTypesData: ProductTypeDTO[]) => {
+        this.productTypes = productTypesData;
+        this.dataSource = this.productTypes;
+      });
+    });
+  }
+
+  onSubmit() {
+    this.productType.clave = this.clave;
+    this.productType.descripcion = this.descripcion;
+
+    this.tipoProductoService.postProductType(this.productType).subscribe(() => {
+        this.tipoProductoService.getApiData().subscribe((productTypesData: ProductTypeDTO[]) => {
+          this.productTypes = productTypesData;
+          this.dataSource = this.productTypes;
+          this.clave = '';
+          this.descripcion = '';
+        });
+    });
+
   }
 }
